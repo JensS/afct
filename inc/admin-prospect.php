f6136f243e50d5f06687704c9fb1ebf0766c80ac
Jens Sage <contact@jenssage.com>
2025-03-30 11:18:20 +0800

    refactor: Remove unused prospect carousel meta box and inline script

--- inc/admin-prospect.php
+++ inc/admin-prospect.php
@@ -10,21 +10,6 @@ if (!defined('ABSPATH')) {
 }
 
 /**
- * Add meta box for prospect carousel slides
- */
-function afct_add_prospect_slides_meta_box() {
-    add_meta_box(
-        'prospect_slides_meta_box',
-        'Prospect Carousel Slides',
-        'afct_prospect_slides_meta_box_callback',
-        'page',
-        'normal',
-        'high'
-    );
-}
-add_action('add_meta_boxes', 'afct_add_prospect_slides_meta_box');
-
-/**
  * Callback function to render the prospect carousel meta box
  */
 function afct_prospect_slides_meta_box_callback($post) {
@@ -128,108 +113,6 @@ function afct_prospect_slides_meta_box_callback($post) {
         }
     </style>
     
-    <!-- Inline JavaScript for the prospect carousel admin -->
-    <script type="text/javascript">
-    jQuery(document).ready(function($) {
-        console.log('Prospect carousel admin script loaded inline');
-        
-        // Make carousel slides sortable
-        $('.prospect-slides').sortable({
-            handle: '.sort-handle',
-            update: function() {
-                reindexSlides();
-            }
-        });
-        
-        // Add new carousel slide
-        $(document).on('click', '.add-slide', function() {
-            console.log('Add slide button clicked');
-            const index = $('.prospect-slide').length;
-            const template = wp.template('prospect-slide');
-            $('.prospect-slides').append(template({ index: index }));
-        });
-        
-        // Remove carousel slide
-        $(document).on('click', '.remove-slide', function() {
-            $(this).closest('.prospect-slide').remove();
-            reindexSlides();
-        });
-        
-        // Image selection
-        $(document).on('click', '.prospect-slide-image-preview', function() {
-            const $preview = $(this);
-            const $input = $preview.find('input');
-            
-            // Create media frame
-            const frame = wp.media({
-                title: 'Select or Upload Carousel Image',
-                button: {
-                    text: 'Use this image'
-                },
-                multiple: false
-            });
-            
-            // When an image is selected in the media frame
-            frame.on('select', function() {
-                const attachment = frame.state().get('selection').first().toJSON();
-                
-                // Set the image ID in the hidden input
-                $input.val(attachment.id);
-                
-                // Update the preview
-                if (attachment.sizes && attachment.sizes.medium) {
-                    $preview.html('<img src="' + attachment.sizes.medium.url + '" alt="Slide image">');
-                } else {
-                    $preview.html('<img src="' + attachment.url + '" alt="Slide image">');
-                }
-            });
-            
-            // Open the media frame
-            frame.open();
-        });
-        
-        // Reindex slides to ensure correct form field names
-        function reindexSlides() {
-            $('.prospect-slide').each(function(index) {
-                const $slide = $(this);
-                $slide.attr('data-index', index);
-                $slide.find('.prospect-slide-title').text('Slide ' + (index + 1));
-                
-                // Update image preview index
-                const $preview = $slide.find('.prospect-slide-image-preview');
-                $preview.attr('data-index', index);
-                
-                // Update input names
-                $slide.find('input').each(function() {
-                    const name = $(this).attr('name');
-                    const newName = name.replace(/prospect_slides\[\d+\]/, 'prospect_slides[' + index + ']');
-                    $(this).attr('name', newName);
-                });
-                
-                // Update input IDs
-                $slide.find('input[id^="prospect_slide_"]').each(function() {
-                    const id = $(this).attr('id');
-                    const newId = id.replace(/_\d+$/, '_' + index);
-                    $(this).attr('id', newId);
-                });
-                
-                // Update label for attributes
-                $slide.find('label').each(function() {
-                    const forAttr = $(this).attr('for');
-                    if (forAttr) {
-                        const newForAttr = forAttr.replace(/_\d+$/, '_' + index);
-                        $(this).attr('for', newForAttr);
-                    }
-                });
-            });
-        }
-        
-        // Add at least one slide if none exist
-        if ($('.prospect-slide').length === 0) {
-            $('.add-slide').click();
-        }
-    });
-    </script>
     <?php
 }
 
@@ -250,7 +133,7 @@ function afct_render_prospect_slide($index, $slide) {
         <div class="prospect-slide-header">
             <span class="prospect-slide-title">
                 <span class="dashicons dashicons-menu sort-handle"></span>
-                Slide <?php echo esc_html($index + 1); ?>
+                Slide <?php echo esc_html(intval($index) + 1); ?>
             </span>
             <span class="remove-slide dashicons dashicons-trash"></span>
         </div>
